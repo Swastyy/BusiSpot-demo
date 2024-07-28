139d365e4bf5780c24b8e0b35b0f30ab5b95a004
Swastika Gupta <gupta.swastika521@gmail.com>
2024-07-28 22:55:24 +0530

    first commit
    first commit

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import { LogoLink, LogoText, LogoWrapper } from './Logo.styled';
+
+export const Logo = () => {
+  return (
+    <LogoWrapper>
+      <LogoLink href="/wheels4rent/">BusiSpot</LogoLink>
+      <LogoText>Find your best rental spot with easy way</LogoText>
+    </LogoWrapper>
+  );
+};
